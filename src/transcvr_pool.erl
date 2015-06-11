e36bded1c17d6dbfd2b79523cba9d7139a250f7a
John <nhf0424@gmail.com>
2015-06-12 01:11:23 +0800

    Debugging...

--- src/transcvr_pool.erl
+++ src/transcvr_pool.erl
@@ -77,7 +77,7 @@ handle_cast(_Msg, State) ->
     {noreply, State}.
 
 handle_info({udp, _Socket, SAddr, SPort, PacketBin}, State) ->
-	fec_server ! {up, {SAddr, SPort}, PacketBin},
+	fec_pool ! {up, {SAddr, SPort}, PacketBin},
 	{noreply, State};
 handle_info(_Info, State) ->
     {noreply, State}.
